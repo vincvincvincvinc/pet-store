cf367ee2b9db8475000ffbd834392e05b1cfe4fa
vincvincvincvinc <152969599+vincvincvincvinc@users.noreply.github.com>
2024-02-22 17:17:46 +0100

    Add entity manager + fix persistence.xml

--- src/main/java/fr/epsi/b32324c1/Main.java
+++ src/main/java/fr/epsi/b32324c1/Main.java
@@ -1,7 +1,13 @@
 package fr.epsi.b32324c1;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
 
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+
+        entityManagerFactory.close();
     }
 }
